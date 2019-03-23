5e329dd21af95dcc9f39d9f52496b4a0beb4d332
ChampionAsh5357 <championash5357@gmail.com>
2019-03-22 20:54:05 -0400

    Blocks and Items

---
+++ src/main/java/com/championash5357/tutorial/item/ItemRuby.java
@@ -0,0 +1,26 @@
+package com.championash5357.tutorial.item;
+
+import java.util.List;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.EnumRarity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+
+public class ItemRuby extends Item {
+
+	public ItemRuby() {
+		super(new Properties().maxStackSize(32).group(ItemGroup.MATERIALS).rarity(EnumRarity.UNCOMMON));
+		this.setRegistryName("ruby");
+	}
+	
+	@Override
+	public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+		tooltip.add(new TextComponentString(TextFormatting.RED + "A shiny more valuable than emerald."));
+	}
+}
